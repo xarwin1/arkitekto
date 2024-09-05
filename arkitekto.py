6b9e0ea8685ecf9540f11a80ea9ca04986425352
Xarwin Gabriel Gloria <xarwin1@github.com>
2024-09-05 18:38:44 +0800

    finished the second stage of installation

--- arkitekto.py
+++ arkitekto.py
@@ -15,7 +15,7 @@ def isDonePartitioning(response):
 		return False
 
 
-def partitionDisk(partitionTool, disk):
+def partitionDisk(disk):
 	os.system("cfdisk " + disk)
 
 def formatDisk(root, boot, swap):
@@ -27,16 +27,45 @@ def mountDisk(root, boot, swap):
 	os.system("mount " + root + " /mnt")
 	os.system("mount --mkdir " + boot + " /mnt/boot")
 	os.system("swapon " + root)
-    os.system("lsblk")
 
 def pacmanKeyInit():
 	os.system("pacman-key --init")
 	os.system("pacman-key --populate archlinux")
 
 def installSystem():
-	os.system("pacstrap -K /mnt base base-devel linux linux-headers
-	linux-firmware vim grub efibootmgr networkmanager")
+	os.system("pacstrap -K /mnt base base-devel linux linux-headers linux-firmware vim grub efibootmgr networkmanager nvim")
 	os.system("genfstab -U /mnt >> /mnt/etc/fstab")
+    os.system("cp -r arkitekto/ /mnt/arkitekto/")
 
 def archChroot():
-	os.system("arch-chroot /mnt ")
+    os.system("arch-chroot /mnt bash -c 'cd /arkitekto;python post-base-installation.py'")
+
+def configureTZandLocales():
+    os.system("ln -sf /usr/local/share/zoneinfo/Asia/Manila")
+    os.system("echo 'LANG=en_US.UTF-8' >> /etc/locale.conf")
+    os.system("echo 'en_US.UTF-8 UTF-8' >> /etc/locale-gen;locale-gen")
+    os.system("hwclock --systohc")
+
+def configureHostname():
+    os.system("echo 'arch' >> /etc/hostname")
+
+def addUser():
+    os.system("useradd -mG wheel xarwin")
+    print("Set user password")
+    os.system("passwd xarwin")
+    print("Set root password")
+    os.system("passwd")
+
+def giveUserSudo():
+    os.system("echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
+
+def installExtraPkgs():
+    os.system("pacman -S $(cat packages.txt |xargs) --needed --noconfirm")
+
+def enableServices():
+    os.system("systemctl enable bluetooth cups NetworkManager gdm")
+
+def installBootloader():
+    os.system("grub-install --target=x86_64-efi --target-directory=/boot --bootloader-id=GRUB")
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
+
---
+++ post-base-installation.py
@@ -0,0 +1,35 @@
+import os
+from arkitekto import *
+import sys
+
+if not isRoot():
+    print("Please run this script as root.")
+    sys.exit()
+
+if not hasInternetConnection():
+    print("You are not connected to any network.")
+    sys.exit()
+
+print("Performing post base installation tasks...")
+
+print("Setting up locale and timezone")
+configureTZandLocales()
+
+print("Setting up hostname")
+configureHostname()
+
+print("Setting up user and privilleges")
+addUser()
+giveUserSudo()
+
+print("Installing extra packages")
+installExtraPackages()
+
+print("Enabling startup services")
+enableServices()
+
+print("Installing bootloader")
+installBootloader()
+
+print("Finished installing. type 'reboot' to reboot the computer")
+os.system("exit")
