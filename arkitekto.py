c927a65ec00567221afaf2ae7fdc477bf0fbd1b1
Xarwin Gabriel Gloria <xarwin1@github.com>
2024-09-07 21:34:54 +0800

    fixed indentation

--- arkitekto.py
+++ arkitekto.py
@@ -39,34 +39,33 @@ def installSystem():
 
 
 def archChroot():
-    	os.system("arch-chroot /mnt bash -c 'cd /arkitekto;python post-base-installation.py'")
+    os.system("arch-chroot /mnt bash -c 'cd /arkitekto;python post-base-installation.py'")
 
 def configureTZandLocales():
-    	os.system("ln -sf /usr/local/share/zoneinfo/Asia/Manila")
-    	os.system("echo 'LANG=en_US.UTF-8' >> /etc/locale.conf")
-    	os.system("echo 'en_US.UTF-8 UTF-8' >> /etc/locale-gen;locale-gen")
-    	os.system("hwclock --systohc")
+    os.system("ln -sf /usr/local/share/zoneinfo/Asia/Manila")
+    os.system("echo 'LANG=en_US.UTF-8' >> /etc/locale.conf")
+    os.system("echo 'en_US.UTF-8 UTF-8' >> /etc/locale-gen;locale-gen")
+    os.system("hwclock --systohc")
 
 def configureHostname():
-    	os.system("echo 'arch' >> /etc/hostname")
+    os.system("echo 'arch' >> /etc/hostname")
 
 def addUser():
-    	os.system("useradd -mG wheel xarwin")
-    	print("Set user password")
-    	os.system("passwd xarwin")
-    	print("Set root password")
-    	os.system("passwd")
+    os.system("useradd -mG wheel xarwin")
+    print("Set user password")
+    os.system("passwd xarwin")
+    print("Set root password")
+    os.system("passwd")
 
 def giveUserSudo():
-    	os.system("echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
+    os.system("echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
 
 def installExtraPkgs():
-    	os.system("pacman -S $(cat packages.txt |xargs) --needed --noconfirm")
+    os.system("pacman -S $(cat packages.txt |xargs) --needed --noconfirm")
 
 def enableServices():
-    	os.system("systemctl enable bluetooth cups NetworkManager gdm")
+    os.system("systemctl enable bluetooth cups NetworkManager gdm")
 
 def installBootloader():
-    	os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
-    	os.system("grub-mkconfig -o /boot/grub/grub.cfg")
-
+    os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
